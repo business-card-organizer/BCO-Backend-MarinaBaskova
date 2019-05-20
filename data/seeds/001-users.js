883ba0cfb6cc35f0e4aa57faf841a7078ff2a3b1
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-20 14:37:31 -0700

    added id for pg in the seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -4,6 +4,7 @@ exports.seed = function(knex, Promise) {
 	return knex('users').del().then(function() {
 		return knex('users').insert([
 			{
+				id: 1,
 				username: 'DavidB',
 				first_name: 'David',
 				last_name: 'Smith',
@@ -11,6 +12,7 @@ exports.seed = function(knex, Promise) {
 				qr_code: Math.random().toString(36).substring(7)
 			},
 			{
+				id: 2,
 				username: 'JohnS',
 				first_name: 'John',
 				last_name: 'Smith',
@@ -18,6 +20,7 @@ exports.seed = function(knex, Promise) {
 				qr_code: Math.random().toString(36).substring(7)
 			},
 			{
+				id: 3,
 				username: 'AmyJ',
 				first_name: 'Amy',
 				last_name: 'Jade',
@@ -29,6 +32,7 @@ exports.seed = function(knex, Promise) {
 				qr_code: Math.random().toString(36).substring(7)
 			},
 			{
+				id: 4,
 				username: 'TomC',
 				first_name: 'Tom',
 				last_name: 'Charles',
@@ -38,6 +42,7 @@ exports.seed = function(knex, Promise) {
 				qr_code: Math.random().toString(36).substring(7)
 			},
 			{
+				id: 5,
 				username: 'JenH',
 				first_name: 'Jen',
 				last_name: 'Hill',
