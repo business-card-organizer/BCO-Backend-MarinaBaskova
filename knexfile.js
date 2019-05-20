caeb0ff35b3ec4dda7c1788963344775457514c0
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-20 12:16:51 -0700

    added knex config for production to deploy on heroku

--- knexfile.js
+++ knexfile.js
@@ -34,20 +34,19 @@ module.exports = {
 				connection.run('PRAGMA foreign_keys = ON', done);
 			}
 		}
+	},
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
 	}
-	// production: {
-	// 	client: 'postgresql',
-	// 	connection: {
-	// 		database: 'my_db',
-	// 		user: 'username',
-	// 		password: 'password'
-	// 	},
-	// 	pool: {
-	// 		min: 2,
-	// 		max: 10
-	// 	},
-	// 	migrations: {
-	// 		tableName: 'knex_migrations'
-	// 	}
-	// }
 };
