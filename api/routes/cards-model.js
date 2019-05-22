81ed231d15531938089bd8550226109936b527df
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-21 20:12:43 -0700

    added get get by id and post routes for cards

--- api/routes/cards-model.js
+++ api/routes/cards-model.js
@@ -1,25 +1,63 @@
-// const db = require('../../config/dbConfig');
+const db = require('../../config/dbConfig');
 
-// module.exports = {
-// 	findAll
-// 	// findById,
-// 	// create,
-// 	// update,
-// 	// remove
-// };
+module.exports = {
+	findAll,
+	findById,
+	create
+	// create,
+	// update,
+	// remove
+};
 
-// async function findAll(user_id) {
-// 	const userCards = await db('cards')
-// 		.select({
-// 			id: 'cards.id',
-// 			firstName: 'cards.first_name',
-// 			lastName: 'cards.last_name',
-// 			organization: 'cards.organization',
-// 			jobTitle: 'cards.job_title',
-// 			email: 'cards.email',
-// 			phone: 'cards.phone',
-// 			eventId: 'cards.event_id'
-// 		})
-// 		.where({ user_id });
-// 	return userCards;
-// }
+async function findAll(user_id) {
+	const userCards = await db('cards')
+		.select({
+			id: 'cards.id',
+			firstName: 'cards.first_name',
+			lastName: 'cards.last_name',
+			organization: 'cards.organization',
+			jobTitle: 'cards.job_title',
+			email: 'cards.email',
+			phone: 'cards.phone',
+			eventId: 'cards.event_id',
+			eventId: 'cards.event_id',
+			eventName: 'events.event_name',
+			city: 'events.city',
+			userId: 'cards.user_id'
+		})
+		.leftOuterJoin('events', 'events.id', 'cards.event_id')
+		.where({ userId: user_id });
+	return userCards;
+}
+
+async function findById(id, userID) {
+	const card = await db('cards')
+		.select({
+			id: 'cards.id',
+			firstName: 'cards.first_name',
+			lastName: 'cards.last_name',
+			organization: 'cards.organization',
+			jobTitle: 'cards.job_title',
+			email: 'cards.email',
+			phone: 'cards.phone',
+			eventId: 'cards.event_id',
+			eventId: 'cards.event_id',
+			eventName: 'events.event_name',
+			city: 'events.city',
+			userId: 'cards.user_id'
+		})
+		.leftOuterJoin('events', 'events.id', 'cards.event_id')
+		.where({ 'cards.id': id })
+		.andWhere({ userId: userID })
+		.orderBy('cards.user_id')
+		.first();
+	return card;
+}
+
+async function create(newCard) {
+	const [ id ] = await db('cards').insert(newCard, 'id');
+	if (id) {
+		const card = await findById(id, newCard.user_id);
+		return card;
+	}
+}
--- api/routes/cards-router.js
+++ api/routes/cards-router.js
@@ -1,21 +1,76 @@
-// const router = require('express').Router();
-// const db = require('./cards-model.js');
+const router = require('express').Router();
+const db = require('./cards-model.js');
+const eventDB = require('./events-model.js');
 
-// // >>>>> /api/cards/
+// >>>>> /api/cards/
 
-// router.get('/', async (req, res) => {
-// 	const userID = req.decodedToken.subject.toString();
+router.get('/', async (req, res) => {
+	const userID = req.decodedToken.subject.toString();
 
-// 	try {
-// 		const cards = await db.findAll(userID);
-// 		if (cards) {
-// 			res.status(200).json(cards);
-// 		} else {
-// 			res.status(404).json({ message: 'User does not have any existing cards.' });
-// 		}
-// 	} catch (error) {
-// 		res.status(500).json({ message: `Event request failed ${error.message}.` });
-// 	}
-// });
+	try {
+		const cards = await db.findAll(userID);
+		if (cards.length > 0) {
+			res.status(200).json(cards);
+		} else {
+			res.status(404).json({ message: 'User does not have any existing cards.' });
+		}
+	} catch (error) {
+		res.status(500).json({ message: `Cards request failed ${error.message}.` });
+	}
+});
 
-// module.exports = router;
+router.get('/:id', async (req, res) => {
+	const userID = req.decodedToken.subject.toString();
+	const { id } = req.params;
+	try {
+		const card = await db.findById(id, userID);
+		if (card) {
+			res.status(200).json(card);
+		} else {
+			res.status(404).json({ message: 'Card with specified ID does not exist.' });
+		}
+	} catch (error) {
+		res.status(500).json({ message: `Cards request failed ${error.message}.` });
+	}
+});
+
+router.post('/', async (req, res) => {
+	const userID = req.decodedToken.subject.toString();
+	const { firstName, lastName, organization, jobTitle, email, phone, eventId } = req.body;
+	if (!firstName || !lastName || !organization || !jobTitle || !email || !phone) {
+		res.status(401).json({
+			message:
+				'Please do not leave firstName,lastName, organization, jobTitle, email or phone of the card fields blank.'
+		});
+	} else {
+		try {
+			let userEvent;
+			if (eventId) {
+				userEvent = await eventDB.findById(eventId, userID);
+			} else {
+				userEvent = null;
+			}
+			try {
+				const newCard = await db.create({
+					first_name: firstName,
+					last_name: lastName,
+					organization: organization,
+					job_title: jobTitle,
+					email: email,
+					phone: phone,
+					event_id: userEvent.id,
+					user_id: userID
+				});
+				if (newCard) {
+					res.status(201).json(newCard);
+				}
+			} catch (error) {
+				res.status(500).json({ message: `Your card could not be posted ${error.message}.` });
+			}
+		} catch (error) {
+			res.status(500).json({ message: `Your card could not be posted ${error.message}.` });
+		}
+	}
+});
+
+module.exports = router;
